e76346e2cf87c8c8c8b5c5221015790e44d7c3b2
Mustafa Can <mustafaacan98@gmail.com>
2022-07-19 20:55:42 +0300

    read more hyperlink added

--- app/src/main/java/com/mustafacan/dailynews/view/NewsListScreen.kt
+++ app/src/main/java/com/mustafacan/dailynews/view/NewsListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
@@ -15,8 +16,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -91,10 +97,9 @@ fun NewsRow(news: NewsItem) {
                     contentDescription = news.title,
                     modifier = Modifier
                         .size(200.dp, 200.dp)
-                        .padding(10.dp)
                 )
-
-                Column(modifier = Modifier.padding(vertical = 10.dp)) {
+                Spacer(modifier = Modifier.padding(10.dp))
+                Column {
                     Text(
                         text = "${news.title}",
                         fontSize = 16.sp,
@@ -106,12 +111,19 @@ fun NewsRow(news: NewsItem) {
                     Spacer(modifier = Modifier.padding(8.dp))
                     Text(text = "Date: ${news.date}")
                 }
-
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(4.dp))
             Text(text = "${news.content}")
             Spacer(modifier = Modifier.padding(8.dp))
-            Text(text = "Read more: ${news.readMoreUrl}")
+            Row(modifier = Modifier.align(Alignment.End)) {
+                HyperlinkText(
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    fullText = "Read more",
+                    linkText = listOf("Read more"),
+                    hyperlinks = listOf("${news.readMoreUrl}"),
+                    fontSize = 14.sp
+                )
+            }
         }
     }
 }
@@ -133,4 +145,61 @@ fun RetryView(error: String, onRetry: () -> Unit) {
             Text(text = "Retry")
         }
     }
+}
+
+@Composable
+fun HyperlinkText(
+    modifier: Modifier = Modifier,
+    fullText: String,
+    linkText: List<String>,
+    linkTextColor: Color = Color.Blue,
+    linkTextFontWeight: FontWeight = FontWeight.Light,
+    linkTextDecoration: TextDecoration = TextDecoration.Underline,
+    hyperlinks: List<String> = listOf(""),
+    fontSize: TextUnit = TextUnit.Unspecified
+) {
+    val annotatedString = buildAnnotatedString {
+        append(fullText)
+        linkText.forEachIndexed { index, link ->
+            val startIndex = fullText.indexOf(link)
+            val endIndex = startIndex + link.length
+            addStyle(
+                style = SpanStyle(
+                    color = linkTextColor,
+                    fontSize = fontSize,
+                    fontWeight = linkTextFontWeight,
+                    textDecoration = linkTextDecoration
+                ),
+                start = startIndex,
+                end = endIndex
+            )
+            addStringAnnotation(
+                tag = "URL",
+                annotation = hyperlinks[index],
+                start = startIndex,
+                end = endIndex
+            )
+        }
+        addStyle(
+            style = SpanStyle(
+                fontSize = fontSize
+            ),
+            start = 0,
+            end = fullText.length
+        )
+    }
+
+    val uriHandler = LocalUriHandler.current
+
+    ClickableText(
+        modifier = modifier,
+        text = annotatedString,
+        onClick = {
+            annotatedString
+                .getStringAnnotations("URL", it, it)
+                .firstOrNull()?.let { stringAnnotation ->
+                    uriHandler.openUri(stringAnnotation.item)
+                }
+        }
+    )
 }
